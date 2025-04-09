9f2b64d0cdca495204c3d4cb31228982fd14e031
Maksym Stoianov <stoianov.maksym@gmail.com>
2025-04-09 19:45:38 +0200

    update Header

--- src/components/Header/ui/Root/styles.ts
+++ src/components/Header/ui/Root/styles.ts
@@ -5,7 +5,6 @@ export const HeaderLink = styled(NavLink)`
   text-decoration: none;
   color: rgba(255, 255, 255, 0.8);
   font-size: 15px;
-  margin-bottom: 12px;
   transition: all 0.3s ease;
   display: block;
   position: relative;
