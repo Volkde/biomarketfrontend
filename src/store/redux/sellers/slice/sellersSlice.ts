165d5888ae2e5952859f49e6dbfb64709d630c23
Maksym Stoianov <stoianov.maksym@gmail.com>
2025-04-09 14:10:40 +0200

    update redux/sellers/fetchCreateSeller

--- src/store/redux/sellers/slice/sellersSlice.ts
+++ src/store/redux/sellers/slice/sellersSlice.ts
@@ -1,3 +1,9 @@
+import { PayloadAction } from "@reduxjs/toolkit";
+import {
+  fetchCreateSeller,
+  Payload as FetchCreateSellerPayload,
+  Result as FetchCreateSellerResult
+} from "shared/api/sellers/fetchCreateSeller";
 import { createAppSlice } from "store/createAppSlice";
 import { SellersState } from "../types/SellersState";
 
@@ -12,6 +18,40 @@ export const sellersSlice = createAppSlice({
   name: "SELLERS",
   initialState,
   reducers: create => ({
+    /**
+     * fetchCreateSeller
+     */
+    fetchCreateSeller: create.asyncThunk<
+      FetchCreateSellerResult,
+      FetchCreateSellerPayload
+    >(
+      async (payload: FetchCreateSellerPayload, { rejectWithValue }) => {
+        try {
+          return await fetchCreateSeller(payload);
+        } catch (error) {
+          return rejectWithValue(error);
+        }
+      },
+      {
+        pending: (state: SellersState) => {
+          state.status = "loading";
+          state.error = initialState.error;
+        },
+        fulfilled: (
+          state: SellersState,
+          { payload }: PayloadAction<FetchCreateSellerResult>
+        ) => {
+          state.status = "success";
+          state.seller = payload.seller;
+          state.error = initialState.error;
+        },
+        rejected: (state: SellersState, { payload }: PayloadAction<any>) => {
+          state.status = "error";
+          state.error = payload?.message ?? "Unknown error";
+        }
+      }
+    ),
+
     /**
      * resetSellersState
      */
