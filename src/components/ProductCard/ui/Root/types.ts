7ab73c6106db94cee267d8f4b01220afc32f9fed
BogdanF <sovtour@gmail.com>
2025-04-14 02:32:35 +0200

    refactor: add JSDoc to ProductCard types

--- src/components/ProductCard/ui/Root/types.ts
+++ src/components/ProductCard/ui/Root/types.ts
@@ -1,5 +1,10 @@
 import { Product } from "types/Product";
 
+/**
+ * Product card component props interface
+ * @interface ProductCardProps
+ * @property {Product} product - Product data
+ */
 export interface ProductCardProps {
   product: Product;
 }
