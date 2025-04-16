129324e9164d760dc5dc381437b71eae0f287025
Genomos <demishev.o@gmail.com>
2025-04-16 19:14:47 +0200

    update Banner section

---
+++ src/pages/Home/ui/Banner/styles.ts
@@ -0,0 +1,53 @@
+import { styled } from "@mui/material/styles";
+
+export const SliderWrapper = styled("div")(({ theme }) => ({
+  width: "100%",
+  maxWidth: "1400px",
+  margin: "0 auto",
+  padding: "0 20px",
+  height: "300px",
+}));
+
+export const Slide = styled("div")<{
+  image: string;
+}>(({ image }) => ({
+  height: "300px",
+  backgroundImage: `url(${image})`,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  borderRadius: "12px",
+  position: "relative",
+}));
+
+export const Overlay = styled("div")(({ theme }) => ({
+  position: "absolute",
+  top: "50%",
+  left: "40px",
+  transform: "translateY(-50%)",
+  color: theme.palette.common.white,
+  maxWidth: "50%",
+}));
+
+export const Title = styled("h2")(({ theme }) => ({
+  fontSize: "2rem",
+  marginBottom: "0.5rem",
+  fontWeight: theme.typography.fontWeightBold,
+}));
+
+export const Subtitle = styled("p")(({ theme }) => ({
+  fontSize: "1.2rem",
+  marginBottom: "1rem",
+}));
+
+export const SlideButton = styled("a")(({ theme }) => ({
+  background: theme.palette.success.main,
+  color: theme.palette.common.white,
+  padding: "10px 20px",
+  textDecoration: "none",
+  borderRadius: "5px",
+  fontWeight: 500,
+  transition: "background 0.3s ease",
+  "&:hover": {
+    background: theme.palette.success.dark,
+  },
+}));
